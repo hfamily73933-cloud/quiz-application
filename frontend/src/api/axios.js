aca965661eefac6a9f8aa05677c4ce58d0e14ae1
Mani Prabhas <maniprabhas639@gmail.com>
2026-03-15 17:12:46 +0530

    Fix API baseURL for production

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://quiz-backend-3079.onrender.com"
+  baseURL: "https://quiz-backend-3079.onrender.com/api"
 });
 
 api.interceptors.request.use((config)=>{
