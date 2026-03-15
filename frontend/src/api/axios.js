6e7e85fdad0b466a0621c2eb0210f292a7f758a3
Mani Prabhas <maniprabhas639@gmail.com>
2026-03-15 17:16:48 +0530

    Use environment variable for API URL

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://quiz-backend-3079.onrender.com/api"
+  baseURL: import.meta.env.VITE_API_URL
 });
 
 api.interceptors.request.use((config)=>{
